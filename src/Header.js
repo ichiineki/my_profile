a1506eccde54cde22a3e1f948f651d1f60137ac8
a.akari300@gmail.com <a.akari300@gmail.com>
2023-05-31 20:53:40 +0900

    first_commit

---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function Header() {
+  return (
+
+      <header className="header">
+        <ul id="menu">
+          <li className="menu_lists">About me</li>
+          <li className="menu_lists">Works</li>
+          <li className="menu_lists"><a href="" className="button">Contact</a></li>
+        </ul>
+      </header>
+
+  );
+}
+
+
+export default Header;
+
---
+++ src/Top.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function Top() {
+  return (
+    <div className="Top">
+        <h2 className="en_header">Takuya Fujikawa</h2>
+        <ul>
+          <li>DESIGNER</li>
+          <li>FRONT-END-ENGINEER</li>
+          <li>VIDEO GRAPHER</li>
+        </ul>
+    </div>
+  );
+}
+
+export default Top;
+
